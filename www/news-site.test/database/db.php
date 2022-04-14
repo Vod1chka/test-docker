fd0ccafe8dbf7f968fc3f6c4306e82b8279a05b0
Aleksandr Dmitrienko <aldmi@opsway.com>
2022-04-14 21:45:55 +0800

    Fix

--- www/news-site.test/database/db.php
+++ www/news-site.test/database/db.php
@@ -1,7 +1,9 @@
 <?php
+
 $mysql = new PDO(
     sprintf('mysql:host=%s;port=%s;dbname=%s;charset=utf8', getenv('MYSQL_HOST'), getenv('MYSQL_PORT'), getenv('MYSQL_DATABASE')),
     getenv('MYSQL_USER'),
     getenv('MYSQL_PASSWORD')
 );
+
 ?>
